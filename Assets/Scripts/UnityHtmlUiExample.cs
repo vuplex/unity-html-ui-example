2d1efa1b6882429eb439521fd93c8e6d23000813
Nathan Hill <binarynate@gmail.com>
2020-10-08 21:09:05 -0400

    Initial release.

---
+++ Assets/Scripts/UnityHtmlUiExample.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using Vuplex.WebView;
+using Vuplex.WebView.Demos;
+
+class UnityHtmlUiExample : MonoBehaviour {
+
+    CanvasWebViewPrefab _canvasWebViewPrefab;
+    HardwareKeyboardListener _hardwareKeyboardListener;
+
+    async void Start() {
+
+        // The CanvasWebViewPrefab's `InitialUrl` property is set via the editor, so it
+        // will automatically initialize itself with that URL.
+        _canvasWebViewPrefab = GameObject.Find("CanvasWebViewPrefab").GetComponent<CanvasWebViewPrefab>();
+
+        _setUpKeyboard();
+        // https://developer.vuplex.com/webview/WebViewPrefab#WaitUntilInitialized
+        await _canvasWebViewPrefab.WaitUntilInitialized();
+
+        // Listen for messages from JavaScript.
+        // https://developer.vuplex.com/webview/IWebView#MessageEmitted
+        // https://support.vuplex.com/articles/how-to-send-messages-from-javascript-to-c-sharp
+        _canvasWebViewPrefab.WebView.MessageEmitted += (sender, eventArgs) => {
+            var message = eventArgs.Value;
+            if (message.StartsWith("auth_success")) {
+                var authToken = message.Split(new char[] {':'}, 2)[1];
+                Debug.Log("Signin succeeded! Auth token: " + authToken);
+            } else if (message.StartsWith("auth_skipped")) {
+                Debug.Log("Signin skipped");
+            }
+        };
+    }
+
+    void _setUpKeyboard() {
+
+        // Enable the native keyboard on mobile.
+        // https://developer.vuplex.com/webview/AndroidWebView#SetNativeKeyboardEnabled
+        #if UNITY_ANDROID && !UNITY_EDITOR
+            AndroidWebView.SetNativeKeyboardEnabled(true);
+        #elif UNITY_IOS && !UNITY_EDITOR
+            iOSWebView.SetNativeKeyboardEnabled(true);
+        #endif
+
+        // Send keys from the hardware keyboard to the webview.
+        _hardwareKeyboardListener = HardwareKeyboardListener.Instantiate();
+        _hardwareKeyboardListener.InputReceived += (sender, eventArgs) => {
+            // Include key modifiers if the webview supports them.
+            var webViewWithKeyModifiers = _canvasWebViewPrefab.WebView as IWithKeyModifiers;
+            if (webViewWithKeyModifiers == null) {
+                _canvasWebViewPrefab.WebView.HandleKeyboardInput(eventArgs.Value);
+            } else {
+                webViewWithKeyModifiers.HandleKeyboardInput(eventArgs.Value, eventArgs.Modifiers);
+            }
+        };
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Unity HTML UI Example
+
+The example Unity project from the blog article [*How to create a UI in Unity using HTML*](https://blog.vuplex.com/how-to-create-unity-ui-with-html).
